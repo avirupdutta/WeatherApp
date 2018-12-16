256fb05c56444ac266c7ae75115d5a0590850d6a
avirupdutta <finalguider99@gmail.com>
2018-12-16 22:58:55 +0530

    almost ready

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 
 $(document).ready(function () { 
   var term;
+  var getlocation = $('#getlocation');    // MAIN LOCATION
+  var locationStatus = $('#mainType');    // WHETHER ITS HAZE, SMOKY, SUNNY .... ETC
+  var showTemp = $('#showTemp');          // WILL SHOW EXACT TEMP
+  var shortNews = $('#shortNews');        // WILL SHOW THE SHORT REPORT SUMMARY
+  var dataTable = $('#dataTable');        // DATA TABLE WITH DETAILED INFO
+
+  // TEMPORARILY HIDING HTML DATA
+
+  function clearScreen() { 
+    getlocation.html('');
+    locationStatus.html('');
+    showTemp.html('');
+    shortNews.html('');
+    dataTable.hide();
+    $('#errorMessage').hide();
+   }
+
+  clearScreen();
 
   $('#location').keypress(function (e) {
     var key = e.which;
@@ -8,6 +26,7 @@ $(document).ready(function () {
         term = $('#location').val();
         document.getElementById('location').value = '';
         if (term != undefined && term != null && term != '' && term != ' ')
+            clearScreen();
             searchLocation(term);
             e.preventDefault();
     }
@@ -17,6 +36,7 @@ $(document).ready(function () {
       term = $('#location').val();        
       document.getElementById('location').value = '';
       if (term != undefined && term != null && term != '' && term != ' ')
+              clearScreen();
               searchLocation(term);
   });
 
@@ -25,13 +45,46 @@ $(document).ready(function () {
     axios({
         method:'get',
         url:`http://api.openweathermap.org/data/2.5/weather?q=${currentLocation}&appid=9fbc7ccff6470a8201aaac9d32862582&units=metric`
-      })
-        .then(function(response) {
-        console.log(response.data.name);
-        console.log(response.data.main.temp + String.fromCharCode(176) + 'C');
-      })
-      .catch(function (error) {
+    }).then(function(response) {
+      
+      var weatherData = response.data; 
+
+      getlocation.html(`${weatherData.name}`);
+      locationStatus.html(`${weatherData.weather[0].main}`);
+      showTemp.html(`${response.data.main.temp + String.fromCharCode(176) }C`);
+      shortNews.html(`Today: ${weatherData.weather[0].main} currently. The low will be ${weatherData.main.temp_min + String.fromCharCode(176)}C and high will be ${weatherData.main.temp_max + String.fromCharCode(176)}C.`);
+      dataTable.show();
+      dataTable.html(`
+      <div class="col text-center details">
+        <ul id="detailTopics">
+            <li>&nbsp;</li>
+            <li>Humidity</li>
+            <li>Pressure</li>
+            <li>Min Temp</li>
+            <li>Max Temp</li>
+            <li>Wind Speed</li>
+        </ul>
+      </div>
+      <div class="col text-center data">
+        <ul id="detailData">
+            <li>&nbsp;</li>
+            <li>${weatherData.main.pressure} pa</li>
+            <li>${weatherData.main.humidity}%</li>
+            <li>${weatherData.main.temp_min + String.fromCharCode(176)}C</li>
+            <li>${weatherData.main.temp_max + String.fromCharCode(176)}C</li>
+            <li>${weatherData.wind.speed} m/s</li>
+        </ul>
+      </div>
+      `);
+      $(this.dataTable).addClass('data-table');
+
+      console.log(response);
+      console.log(response.data.name);
+      console.log(response.data.main.temp + String.fromCharCode(176) + 'C');
+       
+      }).catch(function (error) {
         console.log(error);
+        $('#errorMessage').show();
         $('#errorMessage').html("Sorry, location Not Found! <br><i class=\"far fa-sad-tear\"></i>");
       });
   }
