aaffb218799e2b26b274efedea7fd63fba10aaf6
avirupdutta <finalguider99@gmail.com>
2019-08-19 18:00:53 +0530

    resolved the api bug of http to https in the production

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <title>Weather App</title>
     <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
